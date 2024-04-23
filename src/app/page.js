d23a6096c75f89b9ab3e0ca0f3ae37b1461babab
ouckah <aidan.ouckama@gmail.com>
2024-04-23 17:46:42 -0400

    Landing Page!

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,118 @@
-import Image from "next/image";
+"use client";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <div className="flex flex-col z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <header className="text-5xl font-bold p-5 text-center flex flex-col items-center">
+          Klusters
+        </header>
+        <header className="text-4xl font-bold p-5 text-center">
+          Algorithm Showcase
+        </header>
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
+          <article className="border rounded-lg p-4 flex flex-col items-center">
+            <h2 className="font-semibold text-xl">Clustering Algorithms</h2>
+            <p className="text-md p-2">
+              Discover how clustering algorithms like K-Means, DBSCAN, and
+              Hierarchical Clustering group data into clusters.
+            </p>
+            <button
+              className="mt-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => (window.location.href = "/clustering")}
+            >
+              Learn More
+            </button>
+          </article>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {/* TODO: add these articles */}
+          <article className="border rounded-lg p-4 flex flex-col items-center opacity-50">
+            <h2 className="font-semibold text-xl">Data Structures</h2>
+            <p className="text-md p-2">
+              Explore fundamental data structures like Arrays, Linked Lists, and
+              Trees. (Coming Soon)
+            </p>
+            <button
+              className="mt-auto bg-gray-500 text-white font-bold py-2 px-4 rounded"
+              disabled
+            >
+              Learn More
+            </button>
+          </article>
+          <article className="border rounded-lg p-4 flex flex-col items-center opacity-50">
+            <h2 className="font-semibold text-xl">Sorting Algorithms</h2>
+            <p className="text-md p-2">
+              Explore a variety of sorting algorithms like Quick Sort, Merge
+              Sort, and Bubble Sort. (Coming Soon)
+            </p>
+            <button
+              className="mt-auto bg-gray-500 text-white font-bold py-2 px-4 rounded"
+              disabled
+            >
+              Learn More
+            </button>
+          </article>
+          <article className="border rounded-lg p-4 flex flex-col items-center opacity-50">
+            <h2 className="font-semibold text-xl">Search Algorithms</h2>
+            <p className="text-md p-2">
+              Dive into search algorithms including Binary Search, Linear
+              Search, and more. (Coming Soon)
+            </p>
+            <button
+              className="mt-auto bg-gray-500 text-white font-bold py-2 px-4 rounded"
+              disabled
+            >
+              Learn More
+            </button>
+          </article>
+          <article className="border rounded-lg p-4 flex flex-col items-center opacity-50">
+            <h2 className="font-semibold text-xl">Graph Algorithms</h2>
+            <p className="text-md p-2">
+              Get to know graph algorithms including Depth First Search, Breadth
+              First Search, and more. (Coming Soon)
+            </p>
+            <button
+              className="mt-auto bg-gray-500 text-white font-bold py-2 px-4 rounded"
+              disabled
+            >
+              Learn More
+            </button>
+          </article>
+          <article className="border rounded-lg p-4 flex flex-col items-center opacity-50">
+            <h2 className="font-semibold text-xl">Pathfinding Algorithms</h2>
+            <p className="text-md p-2">
+              Understand how algorithms like A* and Dijkstra's algorithm find
+              the shortest path. (Coming Soon)
+            </p>
+            <button
+              className="mt-auto bg-gray-500 text-white font-bold py-2 px-4 rounded"
+              disabled
+            >
+              Learn More
+            </button>
+          </article>
+          <div className="col-span-full mt-8 text-center">
+            <h3 className="text-2xl font-bold">Welcome to Klusters</h3>
+            <p className="text-md p-2">
+              Your journey into the fascinating world of algorithms begins here.
+              Explore, learn, and master the art of problem-solving through our
+              curated collection of algorithmic challenges and tutorials.
+              Whether you&apos;re a beginner or an experienced coder,
+              there&apos;s something here for everyone. Dive in and start your
+              adventure today!
+            </p>
+            <p className="text-md p-2 mt-4">
+              At Klusters, we believe in the power of algorithms to transform
+              and innovate. Our platform offers a wide range of resources
+              designed to enhance your understanding and application of
+              algorithms. From detailed guides on algorithmic strategies to
+              interactive coding challenges, we provide all the tools you need
+              to become an algorithmic expert. Join our community of passionate
+              coders and embark on a journey of continuous learning and
+              improvement.
+            </p>
+          </div>
+        </section>
       </div>
     </main>
   );
