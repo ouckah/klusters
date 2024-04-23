d1aa75694d2b2d52e21a16aea8aa25a489c4d6c5
ouckah <aidan.ouckama@gmail.com>
2024-04-23 19:53:05 -0400

    Mobile Padding

--- src/app/clustering/page.js
+++ src/app/clustering/page.js
@@ -4,7 +4,7 @@ import { Graph } from "../_components/Graph";
 
 function Clustering() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between md:p-24 p-0">
       <div className="flex flex-col z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <header className="text-5xl font-bold p-5 text-center flex flex-col items-center">
           Clustering Algorithms
